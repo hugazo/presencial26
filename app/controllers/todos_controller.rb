da69215f28704609a22d9d66488e27bfcbfe23a7
Hugo Morales <hmoralesg@icloud.com>
2017-08-03 13:44:42 -0400

    Listo metodo create.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -6,4 +6,16 @@ class TodosController < ApplicationController
   def new
     @todo = Todo.new
   end
+
+  def create
+    @todo = Todo.new(todos_params)
+    @todo.save
+    redirect_to todos_path
+  end
+
+  private
+
+  def todos_params
+    params.require(:todo).permit(:description, :completed)
+  end
 end
